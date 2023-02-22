be3ae3f98b48b636b46f76c0a06c6de21cbb653f
piyush-shaw <piyushshaw0000@gmail.com>
2023-02-22 18:24:35 +0530

    [Piyush] added to insert values in employee payroll table

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -14,3 +14,10 @@ name VARCHAR(200) not null,
 salary money not null,
 start_date date not null
 )
+
+-- UC_03 - Insert value in table
+INSERT into employee_payroll VALUES
+('Piyush',29000,'2023-01-02'),
+('Ashwani',24000,'2022-07-06'),
+('Ankita',32000,'2019-03-18'),
+('Ravi',26000,'2018-05-22')
